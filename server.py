b03fdc8647d8ffb7dcfd848bcb8f5c954a7b5825
Zhussupov Zhassulan <zhzhussupovkz@gmail.com>
2015-02-01 21:15:57 +0600

    add graph rendering

--- server.py
+++ server.py
@@ -9,16 +9,25 @@ def static(path):
 
 @route('/')
 def graph():
+    data = {}
     edges = [Edge('a','b',10), Edge('a','c',3), Edge('c','d',2), Edge('d','b',5), Edge('d','a', 3), Edge('b', 'f',4)]
     myG = Graph(edges=edges)
     myG.add_edge(Edge('e', 'k', 1))\
             .add_edge(Edge('b', 'k', 2))\
             .add_edge(Edge('f', 'e', 3))\
             .add_edge(Edge('e', 'c', 1))
-    return template('views/graph', title = "Graph")
+    data['data'] = myG.to_json()
+    data['title'] = 'Graph'
+    return template('views/graph', data)
 
 @route('/graph')
 def graph():
-    return template('views/graph', title = "Graph")
+    edges = [Edge('a','b',10), Edge('a','c',3), Edge('c','d',2), Edge('d','b',5), Edge('d','a', 3), Edge('b', 'f',4)]
+    myG = Graph(edges=edges)
+    myG.add_edge(Edge('e', 'k', 1))\
+            .add_edge(Edge('b', 'k', 2))\
+            .add_edge(Edge('f', 'e', 3))\
+            .add_edge(Edge('e', 'c', 1))
+    return myG.to_json()
 
 run(host='localhost', port=8080)
--- views/graph.tpl
+++ views/graph.tpl
@@ -10,9 +10,33 @@
 </style>
 </head>
 <body>
-<div id="container">
+<div id="graph-container">
 </div>
+<script src="/static/js/jquery-1.11.2.min.js"></script>
 <script src="/static/js/sigma.min.js"></script>
 <script src="/static/js/sigma.parsers.json.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var g = $.parseJSON($('<div/>').html("{{data}}").text())
+        var nodes = g.nodes;
+        var len = nodes.length;
+        for (var i = 0; i < len; i++) {
+            nodes[i].x = Math.random();
+            nodes[i].y = Math.random();
+        }
+        g.nodes = nodes
+        s = new sigma({
+            graph: g,
+            container: 'graph-container',
+            renderer: {
+                container: document.getElementById('graph-container'),
+                type: 'canvas'
+            },
+            settings: {
+                defaultNodeColor: '#FF0000'
+            }
+        });
+    });
+</script>
 </body>
 </html>
